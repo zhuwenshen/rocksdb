eda6519f2322c35eb83d772db502eb38350af32c
Yanqin Jin <yanqin@fb.com>
2018-12-05 14:57:06 -0800

    Allow file-ingest-triggered flush to skip waiting for write-stall clear (#4751)
    Summary:
    When write stall has already been triggered due to number of L0 files reaching
    threshold, file ingestion must proceed with its flush without waiting for the
    write stall condition to cleared by the compaction because compaction can wait
    for ingestion to finish (circular wait).
    
    In order to avoid this wait, we can set `FlushOptions.allow_write_stall` to be
    true (default is false). Setting it to false can cause deadlock.
    
    This can happen when the number of compaction threads is low.
    
    Considere the following
    ```
    Time  compaction_thread                        ingestion_thread
     |                                             num_running_ingest_file_++
     |    while(num_running_ingest_file_>0){wait}
     |                                             flush
     V
    ```
    Pull Request resolved: https://github.com/facebook/rocksdb/pull/4751
    
    Differential Revision: D13343037
    
    Pulled By: riversand963
    
    fbshipit-source-id: d3b95938814af46ec4c463feff0b50c70bd8b23f

--- db/db_impl.cc
+++ db/db_impl.cc
@@ -2968,6 +2968,7 @@ Status DBImpl::IngestExternalFile(
     }
 
     num_running_ingest_file_++;
+    TEST_SYNC_POINT("DBImpl::IngestExternalFile:AfterIncIngestFileCounter");
 
     // We cannot ingest a file into a dropped CF
     if (cfd->IsDropped()) {
@@ -2983,7 +2984,9 @@ Status DBImpl::IngestExternalFile(
                                &need_flush);
       if (status.ok() && need_flush) {
         mutex_.Unlock();
-        status = FlushMemTable(cfd, FlushOptions(),
+        FlushOptions flush_opts;
+        flush_opts.allow_write_stall = true;
+        status = FlushMemTable(cfd, flush_opts,
                                FlushReason::kExternalFileIngestion,
                                true /* writes_stopped */);
         mutex_.Lock();
