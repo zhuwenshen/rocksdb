76d5a4b3d867201a055742dc9b4471567b9af27a
dorianzheng <xingzhengde72@gmail.com>
2018-12-12 17:52:19 +0800

    Expose column family id to `FlushJobInfo` (#64)
    Summary: Pull Request resolved: https://github.com/facebook/rocksdb/pull/4772
    
    Differential Revision: D13428923
    
    Pulled By: ajkr
    
    fbshipit-source-id: e351e9c5eea97816db25429e129357a8af90712a
    (cherry picked from commit 4862720e08530b42354b328bfd1f6b962d9bedd5)

--- db/db_impl_compaction_flush.cc
+++ db/db_impl_compaction_flush.cc
@@ -221,6 +221,7 @@ void DBImpl::NotifyOnFlushBegin(ColumnFamilyData* cfd, FileMetaData* file_meta,
   mutex_.Unlock();
   {
     FlushJobInfo info;
+    info.cf_id = cfd->GetID();
     info.cf_name = cfd->GetName();
     // TODO(yhchiang): make db_paths dynamic in case flush does not
     //                 go to L0 in the future.
@@ -272,6 +273,7 @@ void DBImpl::NotifyOnFlushCompleted(ColumnFamilyData* cfd,
   mutex_.Unlock();
   {
     FlushJobInfo info;
+    info.cf_id = cfd->GetID();
     info.cf_name = cfd->GetName();
     // TODO(yhchiang): make db_paths dynamic in case flush does not
     //                 go to L0 in the future.
--- include/rocksdb/listener.h
+++ include/rocksdb/listener.h
@@ -142,6 +142,8 @@ struct TableFileDeletionInfo {
 };
 
 struct FlushJobInfo {
+  // the id of the column family
+  uint32_t cf_id;
   // the name of the column family
   std::string cf_name;
   // the path to the newly created file
